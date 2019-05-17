571a36d46cf40e7ae938e6a0b33a2a71799afef2
SgtBagus <procw57@gmail.com>
2019-05-17 13:31:45 +0700

    3.0

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\DB;
 
 class userController extends Controller
 {
@@ -21,18 +23,32 @@ class userController extends Controller
         $query = $request->input('select');
         $limit = $request->input('limit');
         $conditions = $request->input('conditions');
-
-        // die(var_dump($conditions[2]['data'][1]));
+        $current_page = $request->input('current_page');
 
         $data = User::select($query)
         ->whereColumn([
-            ['firstname', '=', $conditions[0]['data'][0]['firstname']],
-            ['lastname', '=', $conditions[0]['data'][0]['lastname']]
+            [$conditions[0]['data'][1][0], $conditions[0]['data'][1][1], $conditions[0]['data'][1][2]],
+            [$conditions[0]['data'][2][0], $conditions[0]['data'][2][1], $conditions[0]['data'][2][2]],
         ])
         ->whereNull($conditions[1]['data'][0])
         ->whereNotIn($conditions[2]['data'][0], $conditions[2]['data'][1])
+        ->whereIn($conditions[3]['data'][0], $conditions[3]['data'][1])
+        ->whereNotBetween($conditions[4]['data'][0], $conditions[4]['data'][1])
+        ->whereBetween($conditions[5]['data'][0], $conditions[5]['data'][1])
+        ->orWhere($conditions[6]['data'][0], $conditions[6]['data'][1], $conditions[6]['data'][2])
+        ->whereNotIn($conditions[7]['function'][0]['data'][0], $conditions[7]['function'][0]['data'][1])
+        ->whereIn($conditions[7]['function'][1]['data'][0], $conditions[7]['function'][1]['data'][1])
         ->take($limit)->get();
 
-        return response($data);
+        $result = [
+            "limit" => $limit,
+            "total_row" => count($data),
+            "total_data" => DB::table('User')->count(),
+            "total_page" => $current_page,
+            "current_page" => $current_page,
+            "data" => response($data)
+        ];
+        return json_encode($result);
+        // return response($data);
     } 
 }
